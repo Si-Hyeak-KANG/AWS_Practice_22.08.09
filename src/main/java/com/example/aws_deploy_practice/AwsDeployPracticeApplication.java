ce1ee2dbdf6a9aae215bf6bfdbc50e9c793b7614
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-08-09 16:19:47 +0900

    first commit

---
+++ src/main/java/com/example/aws_deploy_practice/AwsDeployPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_deploy_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeployPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDeployPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aws_deploy_practice/AwsDeployPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_deploy_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeployPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
