1b8a63384cc71215997bcfbc9023bbdf0f5a1d77
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-08-09 16:21:36 +0900

    add index controller

---
+++ src/main/java/com/example/aws_deploy_practice/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.aws_deploy_practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public @ResponseBody String index() {
+        return "hello world";
+    }
+
+}
